d0187591c4ab10b5d4eeabc778206bedef8fe411
Casey Harding <casey.r.harding@gmail.com>
2021-11-09 12:31:54 -0500

    Simple form in place.

--- src/index.js
+++ src/index.js
@@ -14,9 +14,55 @@ function SimpleForm() {
   const [formValues, setFormValues] = useState({petName: "", petType: ""});
   const [pets, setPets] = useState(petsList);
 
+  const submit = (evt) => {
+    evt.preventDefault();
+    const newPet = {
+      petName: formValues.petName.trim(),
+      petType: formValues.petType.trim()
+    }
+    setPets(pets.concat(newPet));
+    setFormValues({petName: "", petType: ""});
+  }
+
+  const change = (evt) => {
+    const {name, value} = evt.target;
+    setFormValues({ ...formValues, [name]: value});
+  }
+
+  /**
+   * const changeName = (evt) => {
+   *  setFormValues({ petType: formValues.petType, petName: evt.target.value})
+   * }
+   * 
+   * const changeType = (evt) => {
+   * }
+   */
+
   return (
     <div className="container">
       <h1>Simple Form App</h1>
+      {pets.map((pet, idx) => (
+        <div key={idx}>
+          {pet.petName} is a {pet.petType}
+        </div>
+      ))}
+      <form onSubmit={submit}>
+        <input 
+          value={formValues.petName} 
+          name="petName" 
+          type="text" 
+          onChange={change}
+          placeholder="Name ya pet!"
+        />
+        <input
+          value={formValues.petType}
+          name="petType"
+          type="text"
+          onChange={change}
+          placeholder="What is your type of pet?"
+        />
+        <input type="submit" value="Add a pet ya chump!" />
+      </form>
     </div>
   )
 }
